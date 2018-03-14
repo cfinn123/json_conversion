d3e23e2a14f02264a8b813f157add1508cf689a9
Casey Finnicum <casey.finnicum@coyotes.usd.edu>
2018-03-14 17:37:06 -0500

    convert to function

--- conversion_json.py
+++ conversion_json.py
@@ -5,15 +5,20 @@ import csv
 import json
 import sys
 
+# take the file name as the parameter passed to conversion.py
 inFile = sys.argv[1]
 outfile = inFile.strip('.csv')
 
 CSV_PATH = inFile
 JSON_PATH = outfile + str('.json')
 
-with open(CSV_PATH, 'r') as csv_file:
-    reader = csv.DictReader(csv_file)
-    with open(JSON_PATH, 'w') as json_file:
-        for row in reader:
-            json_file.write(json.dumps(row) + '\n')
 
+def csv_to_json(csv_path, json_path):
+    with open(csv_path, 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+        with open(json_path, 'w') as json_file:
+            for row in reader:
+                json_file.write(json.dumps(row) + '\n')
+
+
+csv_to_json(CSV_PATH, JSON_PATH)
